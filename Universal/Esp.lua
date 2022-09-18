cd9d91defffd90f94c6c61a4d2a6b5835f1e9534
zzerexx <74355984+zzerexx@users.noreply.github.com>
2022-09-18 02:41:14 -0700

    Update Esp.lua

--- Universal/Esp.lua
+++ Universal/Esp.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/UniversalEspUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zzerexx/scripts/main/UniversalEspUI.lua", "UniversalEspUI"))()
